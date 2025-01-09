70f7e3a016fea3b021a94806119eda92e6637ab7
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-09 18:27:08 +0900

    Preset制作用メソッド

---
+++ yt-dlp Interface/Brancher/Preset/Preset.cs
@@ -0,0 +1,13 @@
+﻿using yt_dlp_Interface.Brancher.Interfaces;
+using Console = yt_dlp_Interface.Libs.Systems.Console;
+
+namespace yt_dlp_Interface.Brancher.Preset
+{
+    internal class Preset : IOptionSelector
+    {
+        public string Ask() => $"{Console.Ask("Plase enter preset name.")};{ArgumentMaker.MakeArguments()}";
+
+        public Dictionary<string, string> Format(string value) =>
+            new() { { value.Split(';')[0], value.Split(';')[1] } };
+    }
+}
---
+++ yt-dlp Interface/Brancher/PresetMaker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using yt_dlp_Interface.Libs.Systems;
+using Console = yt_dlp_Interface.Libs.Systems.Console;
+
+namespace yt_dlp_Interface.Brancher
+{
+    internal class PresetMaker
+    {
+        private static readonly string settingFile = Path.Combine(Directory.GetCurrentDirectory(), "setting.ydis");
+        private static readonly Libs.Client.Preset presetInterface = new(settingFile);
+        private static readonly Preset.Preset preset = new();
+
+        internal static void Make()
+        {
+            if (!File.Exists(settingFile))
+                File.Create(settingFile);
+            var presetInfo = preset.Format(preset.Ask());
+            if (presetInterface.Setting.Any(presetName => presetInterface.Setting.ContainsKey(presetInfo.Keys.ElementAt(0))))
+                if (!Console.AskYesOrNo("This preset name is already exists.\nCan you execuse override the preset?"))
+                    return;
+            presetInterface.Add(presetInfo.ElementAt(0));
+        }
+    }
+}
