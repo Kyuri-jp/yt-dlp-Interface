40f89e8f055262e436f793df062952ee98f7efb1
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-25 22:05:29 +0900

    IArgumentMakerとISelectorを追加

---
+++ yt-dlp Interface/Commands/ArgumentSelector/Interface/IArgumentMaker.cs
@@ -0,0 +1,7 @@
+﻿namespace yt_dlp_Interface.Commands.ArgumentSelector.Interface
+{
+    internal interface IArgumentMaker<TEnum> where TEnum : Enum
+    {
+        internal Dictionary<TEnum, string> Generate();
+    }
+}
---
+++ yt-dlp Interface/Commands/ArgumentSelector/Interface/ISelector.cs
@@ -0,0 +1,14 @@
+﻿namespace yt_dlp_Interface.Commands.ArgumentSelector.Interface
+{
+    internal interface ISelector
+    {
+        internal enum Mode
+        {
+            Audio,
+            Video,
+            General,
+        }
+
+        internal Mode GetMode();
+    }
+}
