a43ef4be4e2c31bfbc64e986fd8b304503fe5f95
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-26 08:43:45 +0900

    Enum復活

---
+++ yt-dlp Interface/Libs/Object/Enum.cs
@@ -0,0 +1,15 @@
+﻿namespace yt_dlp_Interface.Libs.Object
+{
+    internal static class Enum
+    {
+        internal static TEnum ElementAt<TEnum>(int index) where TEnum : System.Enum
+        {
+            if (index < 0 || index >= System.Enum.GetValues(typeof(TEnum)).Length)
+                throw new ArgumentOutOfRangeException(nameof(index), "Index is out");
+            return (TEnum)System.Enum.GetValues(typeof(TEnum)).GetValue(index)!;
+        }
+
+        internal static TEnum GetEnum<TEnum>(string value) where TEnum : System.Enum =>
+            ElementAt<TEnum>(System.Enum.GetNames(typeof(TEnum)).ToList().IndexOf(value));
+    }
+}
