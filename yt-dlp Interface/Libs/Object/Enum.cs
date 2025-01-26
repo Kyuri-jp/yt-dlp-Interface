e7a64779238909d2b0dcc5624e7d940dbd131e50
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-26 11:06:06 +0900

    Enum.csにContainsメソッドを追加
    Enum.csファイルに新しいメソッドContainsが追加されました。このメソッドは、指定された文字列valueが列挙型TEnumに含まれているかどうかを確認します。looseパラメータがtrueの場合、大文字と小文字を区別せずにチェックします。looseパラメータがfalseの場合、大文字と小文字を区別してチェックします。

--- yt-dlp Interface/Libs/Object/Enum.cs
+++ yt-dlp Interface/Libs/Object/Enum.cs
@@ -11,5 +11,12 @@
 
         internal static TEnum GetEnum<TEnum>(string value) where TEnum : System.Enum =>
             ElementAt<TEnum>(System.Enum.GetNames(typeof(TEnum)).ToList().IndexOf(value));
+
+        internal static bool Contains<TEnum>(string value, bool loose = false) where TEnum : System.Enum =>
+            (loose
+                ? System.Enum.GetNames(typeof(TEnum)).Select(x => x.ToLower())
+                : System.Enum.GetNames(typeof(TEnum))
+            )
+            .Contains(loose ? value.ToLower() : value);
     }
 }
