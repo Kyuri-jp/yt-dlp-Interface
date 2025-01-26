f2ee875f1db54f547afa3b4820890b971938b7cf
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-26 13:14:18 +0900

    使わなくなったクラスを削除

--- yt-dlp Interface/Brancher/BranchChainer.cs
+++
@@ -1,9 +0,0 @@
-﻿using yt_dlp_Interface.Brancher.Interfaces;
-
-namespace yt_dlp_Interface.Brancher
-{
-    internal class BranchChainer
-    {
-        internal static IEnumerable<Dictionary<string, string>> Chain(List<IOptionSelector> optionSelectors) => optionSelectors.Select(@class => @class.Format(@class.Ask()));
-    }
-}
