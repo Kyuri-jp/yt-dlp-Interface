85d7a71be91d4288e44107bac3588d9933adf6af
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-26 10:19:43 +0900

    インスタンス消し忘れ

--- yt-dlp Interface/Applications/ApplicationRunner.cs
+++ yt-dlp Interface/Applications/ApplicationRunner.cs
@@ -1,14 +1,10 @@
-﻿using System.Globalization;
-using yt_dlp_Interface.Applications.Interfaces;
-using yt_dlp_Interface.Libs.Utils;
+﻿using yt_dlp_Interface.Applications.Interfaces;
 using Console = yt_dlp_Interface.Libs.Systems.Console;
 
 namespace yt_dlp_Interface.Applications
 {
     internal class ApplicationRunner
     {
-        private static readonly TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
-
         internal static void RunApplication(string command, Dictionary<IApplication, string> applications)
         {
             var applicationNames = applications.ToList().Select(x => x.Key.GetType().Name.ToLower());
