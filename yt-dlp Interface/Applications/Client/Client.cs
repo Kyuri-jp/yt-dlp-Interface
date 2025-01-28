c7fc03af3cdbeb37f0a8ded790c64eca39c890b0
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-28 22:14:54 +0900

    新しいクラスの追加と `Client` クラスの使用
    `Client.cs` に `Client` クラスを追加し、`IApplication` を実装。
    `Update.cs` に `Update` クラスを追加し、`ICommand` を実装。
    `Version.cs` に `Version` クラスを追加し、`ICommand` を実装。
    `Yt-dlpInterface.cs` に `Client` クラスの使用を追加。

---
+++ yt-dlp Interface/Applications/Client/Client.cs
@@ -0,0 +1,33 @@
+﻿using yt_dlp_Interface.Applications.Interfaces;
+using yt_dlp_Interface.Commands;
+using yt_dlp_Interface.Commands.Interfaces;
+using yt_dlp_Interface.Libs.Systems;
+using Console = yt_dlp_Interface.Libs.Systems.Console;
+
+namespace yt_dlp_Interface.Applications.Client
+{
+    internal class Client : IApplication
+    {
+        public Dictionary<ICommand, string> Commands => new()
+        {
+            { new Commands.Client.Update(), "Update yt-dlp" },
+            { new Commands.Client.Version(), "Check yt-dlp version" }
+        };
+
+        void IApplication.Run()
+        {
+            while (true)
+            {
+                string command = Console.AskLikeCui("Preset");
+                if (command == "exit")
+                    break;
+                if (command.Equals("help", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    ShowHelp<ICommand>.ShowHelps(Commands);
+                    continue;
+                }
+                CommandRunner.RunCommand(command, Commands.ToDictionary());
+            }
+        }
+    }
+}
