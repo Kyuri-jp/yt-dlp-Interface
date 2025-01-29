a5a25c323e0711ebf5daef39a23855b739109612
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-01-29 19:45:52 +0900

    Add IOptionFormatter

---
+++ yt-dlp Interface/Commands/OptionSelector/Interface/IOptionFormatter.cs
@@ -0,0 +1,7 @@
+﻿namespace yt_dlp_Interface.Commands.OptionSelector.Interface
+{
+    internal interface IOptionFormatter
+    {
+        internal string Format(Dictionary<string, string> data);
+    }
+}
