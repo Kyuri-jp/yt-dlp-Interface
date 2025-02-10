1a6778a5336dd980151a81a1732ed4549ad6a26c
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-02-10 23:42:54 +0900

    Optionの種類がFormatになってた

--- yt-dlp Interface/Commands/OptionSelector/Audio/Thumbnail.cs
+++ yt-dlp Interface/Commands/OptionSelector/Audio/Thumbnail.cs
@@ -11,7 +11,7 @@ namespace yt_dlp_Interface.Commands.OptionSelector.Audio
         Dictionary<string, string> IOptionSelector.Generate() => new()
         {
             {
-                GeneratedOptions.Format.ToString(),
+                GeneratedOptions.Thumbnail.ToString(),
                 Console.AskYesOrNo("Will you embed thumbnail?").ToString()
             }
         };
