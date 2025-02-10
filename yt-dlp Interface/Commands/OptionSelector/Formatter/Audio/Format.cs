336af194b8c94521b6a248c0f14f6b79b5e071d0
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-02-10 23:27:12 +0900

    Format(Audio)のFormatterを設定

--- yt-dlp Interface/Commands/OptionSelector/Formatter/Audio/Format.cs
+++ yt-dlp Interface/Commands/OptionSelector/Formatter/Audio/Format.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using yt_dlp_Interface.Commands.OptionSelector.Interface;
+﻿using yt_dlp_Interface.Commands.OptionSelector.Interface;
 
 namespace yt_dlp_Interface.Commands.OptionSelector.Formatter.Audio
 {
     internal class Format : IOptionFormatter
     {
-        string IOptionFormatter.Format(Dictionary<string, string> data)
-        {
-            throw new NotImplementedException();
-        }
+        string IOptionFormatter.Format(Dictionary<string, string> data) => $"--audio-format {data[$"{Options.AudioOptions.Format}"]}";
 
         Options.GeneratedOptions IOptionFormatter.GetGeneratedOption() => Options.GeneratedOptions.Format;
     }
