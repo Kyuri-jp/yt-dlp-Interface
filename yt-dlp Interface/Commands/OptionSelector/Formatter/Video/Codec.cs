ca53bc1f61a61b814e9835ed7eb6c8ad7ddf4e48
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-02-10 23:22:03 +0900

    CodecのFormatterを設定

--- yt-dlp Interface/Commands/OptionSelector/Formatter/Video/Codec.cs
+++ yt-dlp Interface/Commands/OptionSelector/Formatter/Video/Codec.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using yt_dlp_Interface.Commands.OptionSelector.Interface;
+﻿using yt_dlp_Interface.Commands.OptionSelector.Interface;
 
 namespace yt_dlp_Interface.Commands.OptionSelector.Formatter.Video
 {
     internal class Codec : IOptionFormatter
     {
-        string IOptionFormatter.Format(Dictionary<string, string> data)
-        {
-            throw new NotImplementedException();
-        }
+        string IOptionFormatter.Format(Dictionary<string, string> data) => $"-S vcodec:{data[$"{Options.VideoOptions.Codec}"]}";
 
         Options.GeneratedOptions IOptionFormatter.GetGeneratedOption() => Options.GeneratedOptions.Codec;
     }
