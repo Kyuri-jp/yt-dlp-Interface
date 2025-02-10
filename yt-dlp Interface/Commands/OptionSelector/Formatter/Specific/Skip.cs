c4bb30b49b7063a1fe1c1e0cc91cc0c89c85e941
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-02-10 23:22:28 +0900

    Skipのusing消し

--- yt-dlp Interface/Commands/OptionSelector/Formatter/Specific/Skip.cs
+++ yt-dlp Interface/Commands/OptionSelector/Formatter/Specific/Skip.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using yt_dlp_Interface.Commands.OptionSelector.Interface;
+﻿using yt_dlp_Interface.Commands.OptionSelector.Interface;
 
 namespace yt_dlp_Interface.Commands.OptionSelector.Formatter.Specific
 {
