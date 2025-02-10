a25dd487423b20e44065b4012007425bca54add4
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-02-10 23:25:43 +0900

    MetadataとThumbnailの引数間違えてた

--- yt-dlp Interface/Commands/OptionSelector/Formatter/Audio/Metadata.cs
+++ yt-dlp Interface/Commands/OptionSelector/Formatter/Audio/Metadata.cs
@@ -4,7 +4,7 @@ namespace yt_dlp_Interface.Commands.OptionSelector.Formatter.Audio
 {
     internal class Metadata : IOptionFormatter
     {
-        string IOptionFormatter.Format(Dictionary<string, string> data) => $"{Options.AudioOptions.Metadata}";
+        string IOptionFormatter.Format(Dictionary<string, string> data) => "--embed-metadata";
 
         Options.GeneratedOptions IOptionFormatter.GetGeneratedOption() => Options.GeneratedOptions.Metadata;
     }
--- yt-dlp Interface/Commands/OptionSelector/Formatter/Audio/Thumbnail.cs
+++ yt-dlp Interface/Commands/OptionSelector/Formatter/Audio/Thumbnail.cs
@@ -4,7 +4,7 @@ namespace yt_dlp_Interface.Commands.OptionSelector.Formatter.Audio
 {
     internal class Thumbnail : IOptionFormatter
     {
-        string IOptionFormatter.Format(Dictionary<string, string> data) => $"{Options.AudioOptions.Thumbnail}";
+        string IOptionFormatter.Format(Dictionary<string, string> data) => "--embed-thumbnail";
 
         Options.GeneratedOptions IOptionFormatter.GetGeneratedOption() => Options.GeneratedOptions.Thumbnail;
     }
