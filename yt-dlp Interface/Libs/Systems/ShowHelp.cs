9aabc5e099be23811be84919563f8d34fca43a28
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-02-11 10:30:46 +0900

    ShowHelpクラスにShowCommandHelpsメソッドを追加
    - `ShowHelp` クラスに `ShowCommandHelps` メソッドを追加
      - コマンド名とその引数を表示
      - 存在するコマンドの引数を緑色で表示
      - 存在しない場合は「Invalid command」を赤色で表示

--- yt-dlp Interface/Libs/Systems/ShowHelp.cs
+++ yt-dlp Interface/Libs/Systems/ShowHelp.cs
@@ -1,8 +1,19 @@
-﻿namespace yt_dlp_Interface.Libs.Systems
+﻿using yt_dlp_Interface.Commands.Interfaces;
+
+namespace yt_dlp_Interface.Libs.Systems
 {
     internal class ShowHelp<TInterface> where TInterface : notnull
     {
         internal static void ShowHelps(Dictionary<TInterface, string> datas) =>
             datas.ToList().ForEach(data => Console.ColoredWriteLine($"{data.Key.GetType().Name} : {data.Value}", ConsoleColor.Green));
+
+        internal static void ShowCommandHelps(Dictionary<ICommand, string> datas, string command)
+        {
+            if (datas.Keys.Select(x => x.GetType().Name).Contains(command))
+                datas.ToList().Find(x => x.Key.GetType().Name == command).Key.Arguments.ToList()
+                     .ForEach(x => Console.ColoredWriteLine($"{x.Key} : {x.Value}", ConsoleColor.Green))
+            else
+                Console.ColoredWriteLine("Invalid command", ConsoleColor.Red);
+        }
     }
 }
