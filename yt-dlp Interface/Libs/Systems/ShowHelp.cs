a3d011961ba8ff0f2447aee7cbf326e28c77745d
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2025-02-11 10:39:19 +0900

    Add new ShowHelps method and modify ShowCommandHelps
    新しいオーバーロードの ShowHelps メソッドを ShowHelp クラスに追加しました。このメソッドは、Dictionary<ICommand, string> 型の datas と Dictionary<string, List<string>> 型の argumentData を引数に取ります。argumentData の中でキーが "more" であるエントリを探し、そのエントリの値のリストが空でない場合は、ShowCommandHelps メソッドを呼び出します。そうでない場合は、datas の内容をコンソールに出力します。既存の ShowCommandHelps メソッドはプライベートメソッドに変更されました。

--- yt-dlp Interface/Libs/Systems/ShowHelp.cs
+++ yt-dlp Interface/Libs/Systems/ShowHelp.cs
@@ -7,7 +7,15 @@ namespace yt_dlp_Interface.Libs.Systems
         internal static void ShowHelps(Dictionary<TInterface, string> datas) =>
             datas.ToList().ForEach(data => Console.ColoredWriteLine($"{data.Key.GetType().Name} : {data.Value}", ConsoleColor.Green));
 
-        internal static void ShowCommandHelps(Dictionary<ICommand, string> datas, string command)
+        internal static void ShowHelps(Dictionary<ICommand, string> datas, Dictionary<string, List<string>> argumentData)
+        {
+            if (argumentData.Skip(1).Where(x => x.Key == "more").First().Value.Count > 0)
+                ShowCommandHelps(datas, argumentData.Skip(1).Where(x => x.Key == "more").First().Value[0]);
+            else
+                datas.ToList().ForEach(data => Console.ColoredWriteLine($"{data.Key.GetType().Name} : {data.Value}", ConsoleColor.Green));
+        }
+
+        private static void ShowCommandHelps(Dictionary<ICommand, string> datas, string command)
         {
             if (datas.Keys.Select(x => x.GetType().Name).Contains(command))
                 datas.ToList().Find(x => x.Key.GetType().Name == command).Key.Arguments.ToList()
